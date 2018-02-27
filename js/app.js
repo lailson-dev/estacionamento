ea5b33a809e99ed105a0334f2839f8aced9f8056
Lailson Conceição <Lailsoncv@hotmail.com>
2018-02-27 16:59:55 -0300

    Criação do método de listagem do pátio e listagem na index

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@
     doc.querySelector('[data-js="formulario"]').addEventListener('submit', cadastraVeiculo);
     // Função de cadastro de veículos
     function cadastraVeiculo(event) {
-        // Isso aqui todo mundo já sabe. Faz com que o comportamento padrão do formulário seja cancelado
-        event.preventDefault();
         // Seleção dos inputs
         var modeloVeiculo = doc.querySelector('[data-js="modeloVeiculo"]').value;
         var placaVeiculo = doc.querySelector('[data-js="placaVeiculo"]').value;
@@ -30,10 +28,40 @@
             // Atribuo à variável carros os valores que tem lá no LocalStorage
             // Lembrando de converter o formato json para string.
             var carros = JSON.parse(localStorage.getItem('patio'));
+            carros.push(carro);
             localStorage.setItem('patio', JSON.stringify(carros));
         }
+        // Executo a função para que todas as vezes que forem cadastrados novos carros
+        // Atualize a tabela automaticamente
+        mostraPatio();
+        // Isso aqui todo mundo já sabe. Faz com que o comportamento padrão do formulário seja cancelado
+        event.preventDefault();
+    } // FINAL da Função de cadastro de veículos
 
-        console.log(carro);
-    }
+    // Função para listar os carros cadastrados no pátio
+    function mostraPatio() {
+        // Pegando os dados do pátio e atribuindo a variável
+        var carros = JSON.parse(localStorage.getItem('patio'));
+        // Selecionando a tabela lá da index
+        var carrosResultado = doc.querySelector('[data-js="resultados"]');
+        // Setando as informações da variável carrosResultados à tabela na index
+        // Inicia em null porque vamos setar os valores quando iterar
+        carrosResultado.innerHTML = '';
+        // Iteração
+        for (var i = 0; i < carros.length; i++) {
+            var modelo = carros[i].modelo;
+            var placa = carros[i].placa;
+            var hora = carros[i].hora;
+            var minutos = carros[i].minutos;
+
+            // Agora atribuo o resultado ao carrosResultados.innerHTML
+            carrosResultado.innerHTML += '<tr><td>' + modelo +
+                                         '</td><td>' + placa +
+                                         '</td><td>' + hora + ':' + minutos +
+                                         '</td></tr>';
+        }
+    }// FINAL da Função para listar os carros cadastrados no pátio
+    // Adiciono ao body da index o evento onload passando a função para carregar a tabela
+    doc.body.addEventListener('onload', mostraPatio());
 
 })(window, document);
